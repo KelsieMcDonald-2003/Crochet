de10769087d87ac33a670edd7e8a2cf9521a94ab
KelsieMcDonald-2003 <kelsie.lin.mcdonald@gmail.com>
2024-04-07 19:28:14 -0700

    Created an Image Carousel on the home's page that's made up of any photos that were uploaded by users.

--- Crochet/Crochet/Controllers/HomeController.cs
+++ Crochet/Crochet/Controllers/HomeController.cs
@@ -1,21 +1,33 @@
-﻿using Crochet.Models;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 using System.Diagnostics;
+using Crochet.Data;
+using Crochet.Data.Repositories;
+using Crochet.Models;
 
 namespace Crochet.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext context;
+        private readonly IPhotosRepository prepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext c, IPhotosRepository p)
         {
             _logger = logger;
+            context = c;
+            prepository = p;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var photos = prepository.GetPhotos();
+            return View(photos);
         }
 
         public IActionResult Privacy()
